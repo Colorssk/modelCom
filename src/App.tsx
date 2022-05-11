58beb59ed4a233283ef27e6c01e4f79efff15034
Colorssk <1043075409@qq.com>
2022-05-12 00:22:46 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LText from './components/LText'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LText>asdsa</LText>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LText/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LText(props: any) {
+  return (
+    <div>
+        {props.children}
+    </div>
+  );
+}
