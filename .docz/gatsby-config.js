9cb307a62deedc001a142ced10f80289593d9d11
ming.yan <ming.yan@sumscope.com>
2022-06-28 13:28:59 +0800

    fix: style

--- .docz/gatsby-config.js
+++ .docz/gatsby-config.js
@@ -68,7 +68,7 @@ const config = {
         title: '组件库左上角标题',
         description: 'My awesome app using docz',
         host: 'localhost',
-        port: 3001,
+        port: 3000,
         p: 3000,
         separator: '-',
         paths: {
