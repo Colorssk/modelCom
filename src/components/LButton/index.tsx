90cf40a81aa5fd619edd25ed069e8a1e377914d2
ming.yan <ming.yan@sumscope.com>
2022-07-11 11:11:35 +0800

    create model1

--- src/components/LButton/index.tsx
+++ src/components/LButton/index.tsx
@@ -1,7 +1,42 @@
-import "./index.less";
-import React from "react";
-export default function LButton(props: any) {
-  return (
-    <button className="LButton-container">{props.children}</button>
-  );
-}
+import { Button, Tooltip } from 'antd';
+import React from 'react';
+import './themeStyle/index.less';
+
+const LButton = ({
+  className,
+  type,
+  disabled,
+  tooltip,
+  width,
+  ...restProps
+} : any) => {
+  let typeClass = 'u-button-wrapper-basic';
+  if (disabled) {
+    type = 'gray';
+  }
+  if (type) {
+    switch (type) {
+    case 'gray':
+      typeClass = 'u-button-wrapper-gray';
+      break;
+    case 'blue':
+      typeClass = 'u-button-wrapper-blue';
+      break;
+    }
+  }
+  return <div className={[ 'u-button-wrapper', typeClass ].join(' ')}>
+    <Tooltip title={tooltip}>
+      <Button
+        size="small"
+        style={{ width }}
+        disabled={disabled}
+        className={className}
+        {...restProps}
+      >
+        {restProps.children}
+      </Button>
+    </Tooltip>
+  </div>;
+};
+
+export default LButton;
