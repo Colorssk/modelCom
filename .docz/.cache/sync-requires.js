dedc5125d70f4e1ab65248f31e85cb3f95ff677f
ming.yan <ming.yan@sumscope.com>
2022-07-18 12:46:21 +0800

    update docsite

--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("C:\\unite-project\\获取template模板\\modelCom\\.docz\\.cache\\dev-404-page.js"))),
   "component---src-components-index-mdx": (preferDefault(require("C:\\unite-project\\获取template模板\\modelCom\\src\\components\\index.mdx"))),
   "component---src-components-l-button-index-mdx": (preferDefault(require("C:\\unite-project\\获取template模板\\modelCom\\src\\components\\LButton\\index.mdx"))),
   "component---src-pages-404-js": (preferDefault(require("C:\\unite-project\\获取template模板\\modelCom\\.docz\\src\\pages\\404.js")))
--- .docz/gatsby-config.js
+++ .docz/gatsby-config.js
@@ -68,7 +68,7 @@ const config = {
         title: 'component ui',
         description: 'My awesome app using docz',
         host: 'localhost',
-        port: 3000,
+        port: 3001,
         p: 3000,
         separator: '-',
         paths: {
